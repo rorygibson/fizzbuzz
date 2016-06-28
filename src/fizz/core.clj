2cfbb51586ce97e9ee10877fd8d702550c714918
Rory Gibson <rory.gibson@brightnorth.co.uk>
2016-06-28 13:47:48 +0100

    Doco, and missed method rename

--- src/fizz/core.clj
+++ src/fizz/core.clj
@@ -2,7 +2,7 @@
   (:require [clojure.string :refer [index-of]]))
 
 
-(defn divisible-by [n d] (and (number? n) (= 0 (mod n d))))
+(defn number-divisible-by [n d] (and (number? n) (= 0 (mod n d))))
 
 
 (defn fizz [n]     (if (number-divisible-by n 3) "Fizz" n))
@@ -12,12 +12,13 @@
 
 
 (defn translate
-  "Translate a sequence of numbers to a FizzBuzz/Lucky string (on one line)"
+  "Translate a sequence of elements to a FizzBuzz/Lucky string (on one line)"
   [n]
   (some-> n fizzbuzz lucky fizz buzz))
 
 
 (defn process
+  "Process some input into a FizzBuzz string."
   [ns]
   (if (sequential? ns)
     (apply str (map translate ns))
